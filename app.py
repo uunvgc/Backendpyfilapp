5ece0115f3935ebb063e90529df1839af042185d
uunvgc <Keithross1205@gmail.com>
2026-02-26 17:48:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -404,4 +404,4 @@ start_worker_if_enabled()
 # Optional: allow local run
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True) 
